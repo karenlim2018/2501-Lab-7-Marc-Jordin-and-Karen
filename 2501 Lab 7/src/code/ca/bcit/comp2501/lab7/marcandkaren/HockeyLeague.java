fafad0d0ee342c23cdebec9f67f6b959541abeaf
karenlim2018 <38368613+karenlim2018@users.noreply.github.com>
2024-05-29 19:08:07 -0700

    Reorganized the files into code/test folders

---
+++ 2501 Lab 7/src/code/ca/bcit/comp2501/lab7/marcandkaren/HockeyLeague.java
@@ -0,0 +1,14 @@
+package ca.bcit.comp2501.lab7.marcandkaren;
+
+/**
+ * Description
+ *
+ * @author Karen Lim
+ * @author Marc-André Baron-Campeau
+ *
+ * @version 1.0
+ */
+public class HockeyLeague
+{
+
+}
---
+++ 2501 Lab 7/src/test/ca/bcit/comp2501/lab7/marcandkaren/HockeyLeagueTest.java
@@ -0,0 +1,20 @@
+package ca.bcit.comp2501.lab7.marcandkaren;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HockeyLeagueTest
+{
+
+    @BeforeEach
+    void setUp()
+    {
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+    }
+}
